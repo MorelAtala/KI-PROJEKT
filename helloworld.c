ff7cc14ab217e0a3a747ff2010c641bbc7754aee
MorelAtala <75019641+MorelAtala@users.noreply.github.com>
2021-11-04 18:15:47 +0100

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -4,59 +4,103 @@
 #include <stdio.h>
 #include <regc515c.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 //Diese Include-Datei macht alle Funktionen der
 //AkSen-Bibliothek bekannt.
 //Unbedingt einbinden!
 #include <stub.h>
 
+void folgelinie();
+void rechts();
+
 //Hauptprogrammroutine
 void AksenMain(void)
 {
-	
-	unsigned char wert = 0, wert_links=0, wert_recht=0;
-	lcd_cls();
-	
-while(1) {
-		wert_links = analog(0); 
-		wert_recht = analog(7);
 
-		if (wert_links<100 && wert_recht<100)
+	folgelinie();
+	
+
+	//	sleep(200);  motor 3 links motor 1 recht
+
+
+
+	
+}
+
+
+void folgelinie()
+{
+	unsigned char wert_links = 0, wert_recht = 0, wert_mitte = 0;
+	lcd_cls();
+
+	while (1) {
+		wert_recht = analog(0);
+		wert_links = analog(7);
+		wert_mitte = analog(2);
+
+		if (wert_links > 100 && wert_recht > 100 && wert_mitte > 100)
+
+		{
+			rechts();
+		}
+
+		if (wert_links<100 && wert_recht<100 && wert_mitte > 100)
+		{
+			motor_richtung(3, 0);
+			motor_richtung(1, 0);
+			motor_pwm(3, 8);
+			motor_pwm(1, 8);
+		}
+		/*else if (wert_links>100 && wert_recht>100)
 		{
 			motor_richtung(3, 0);
 			motor_richtung(1, 0);
 			motor_pwm(3, 10);
 			motor_pwm(1, 10);
-		}
-		else if (wert_links>100 && wert_recht>100)
+		} */
+		else if (wert_links>100 && wert_recht<100 && wert_mitte < 100)
 		{
 			motor_richtung(3, 0);
+			motor_pwm(3, 5);
 			motor_richtung(1, 0);
-			motor_pwm(3, 10);
-			motor_pwm(1, 10);
+			motor_pwm(1, 2);
+			
 		}
-		else if (wert_links>100 && wert_recht<100)
+		else if (wert_links<100 && wert_recht>100 && wert_mitte < 100)
 		{
 			motor_richtung(3, 0);
 			motor_pwm(3, 2);
 			motor_richtung(1, 0);
-			motor_pwm(1, 7);
+			motor_pwm(1, 5);
+
 		}
-		else if (wert_links<100 && wert_recht>100)
-		{
-			motor_richtung(3, 0);
-			motor_pwm(3, 7);
-			motor_richtung(1, 0);
-			motor_pwm(1, 2);
-		}
+
+	}
+
+
+}
+
+
+void rechts()
+
+{
+	unsigned char wert_links = 0, wert_recht = 0, wert_mitte = 0;
+	wert_recht = analog(0);
+	wert_links = analog(7);
+	wert_mitte = analog(2);
 	
-	} 
-
-
-
-//	sleep(200);  motor 3 links motor 1 recht
-
-
-
-	while (1);
+	
+		motor_richtung(3, 0);
+		motor_richtung(1, 0);
+		motor_pwm(3, 0);
+		motor_pwm(1, 8);
+		sleep(1000);
+		while (wert_mitte < 100)
+		{
+			wert_mitte = analog(2);
+		}
+		
+		
+	
 }
