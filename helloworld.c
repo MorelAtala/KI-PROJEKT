889df75d8ccc03b8e3f313a6503fee9b26f545af
MorelAtala <75019641+MorelAtala@users.noreply.github.com>
2022-01-06 16:57:13 +0100

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -24,7 +24,14 @@ unsigned char i = 0;
 //unsigned char Plan[] = "RLGLRGRLGLLG.";
 unsigned char Plan[] = "RLLRRRRL.";
 unsigned char wert_links = 0, wert_recht = 0, wert_mitte = 0;
-
+unsigned char x, y, xs = 0, ys = 0, k;
+//unsigned char heuristicherweg[20];
+//unsigned char richtigerweg[] = "HHHHHHHH";
+unsigned char Fahrplan[] =
+"xFxxxFxx.x.x.xF..x..Fxx.x.xxx..x..xx.x.x.xF..x..Fxx.x.xxx..x..xx.x.x.x";
+unsigned char nachbar[] = "SWNOSW";
+unsigned char Gitter[10][7];
+unsigned char counter = 0;
 void AksenMain(void)
 {
 	while (1) {
@@ -42,64 +49,22 @@ void AksenMain(void)
 
 void weg()
 {
-	//unsigned char wert_links = 0, wert_recht = 0, wert_mitte = 0;
-	lcd_cls();
-
-	wert_recht = analog(0);
-	wert_links = analog(7);
-	wert_mitte = analog(2);
-
-
-
-
-	while (i < 9)
-	{
-
-		if (wert_links > 100 && wert_recht > 100 && wert_mitte > 100)
-		{
-
-			if (Plan[i] == 'R')
-			{
-				rechts();
-				i++;
-			}
-			else if (Plan[i] == 'L')
-			{
-				links();
-				i++;
-			}
-			else if (Plan[i] == 'G')
-			{
-				geradeaus();
-				i++;
-			}
-			else
-			{
-				stop();
-				i++;
-			}
-
-
-		}
-		else
-
-		{
-			folgelinie();
-		}
-
-		
-		
-	}
 	
-
-	/*	else
-		{
-		folgelinie();
+for (x = 0; x <= 10; x++)
+	 {
+		 for (y = 0; y < 7; y++)
+			 {
+			 if (Fahrplan[i] == ’x’)
+				 Gitter[x][y] = 70;
+			else if (Fahrplan[i] == ’.’)
+				 Gitter[x][y] = 71;
+			 else if (Fahrplan[i] == ’F’)
+				 Gitter[x][y] = 72;
+			 else if (Fahrplan[i] == ’#’)
+				 Gitter[x][y] = 73;
+			 i++;
+			 }
 		}
-		*/
-
-	stop();
-
 }
 
 
@@ -226,3 +191,37 @@ void stop()
 
 }
 
+
+ void umdrehen() {
+	 motor_richtung(3, 0);
+	 motor_richtung(1, 0);
+	 motor_pwm(3, 0);
+	 motor_pwm(1, 7);
+	 weg();
+	 sleep(1000);
+	 while (analog(2) < 100);
+	 motor_pwm(1, 0);
+	 count++;
+	 folgelinie();
+	
+}
+
+
+ void liefern() {
+	folgelinie();
+	motor_pwm(3, 0);
+	motor_pwm(1, 0);
+	open();
+	umdrehen();
+	sleep(900);
+	motor_richtung(3, 1);
+	motor_richtung(1, 1);
+	motor_pwm(3, 7);
+	motor_pwm(1, 7);
+	sleep(200);
+	motor_pwm(3, 0);
+	motor_pwm(1, 0);
+	close();
+	rechts();
+	
+}
